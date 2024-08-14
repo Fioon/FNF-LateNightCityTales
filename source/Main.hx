79ea1a18bf08597085cef700d8251b61bb036e06
Fioon <135500399+Fioon@users.noreply.github.com>
2024-08-14 17:25:23 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -78,7 +78,7 @@ class Main extends Sprite
 
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, #if (flixel < "5.0.0") zoom, #end framerate, framerate, skipSplash, startFullscreen));
 
-		#if !mobile
+		#if mobile
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		Lib.current.stage.align = "tl";
